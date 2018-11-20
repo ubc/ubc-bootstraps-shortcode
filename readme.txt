bbc9e7969a1514b277c792929a923656795e72ac
Richard Tape <richard.tape@ubc.ca>
2018-11-20 12:49:24 -0800

    Added initial commit

---
+++ readme.txt
@@ -0,0 +1,108 @@
+=== Bootstraps-shortcode ===
+Contributors: brabbins
+Version: 2
+Donate Link: N/A
+Tags: bootstrap, shortcodes,
+Requires at least: 3.0.1
+Tested up to: 3.4
+Stable tag: 4.3
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+A plugin that implements shortcode that utilizes some of bootstraps styles. 
+
+== Description ==
+
+A plugin that implements shortcode that utilizes some of bootstraps styles. 
+
+
+
+== Installation ==
+
+1. Upload `bootstrap-shortcodes` folder to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+
+**Uses**
+
+
+**Colours**
+[colors]Content[/colours]
+
+**Alerts**
+[alert]Content[/alert]
+[alert name="success"]Content[/alert]
+See all the alerts name attributes at
+http://twitter.github.io/bootstrap/components.html#alerts
+
+**Button with built in link**
+[button link="http://link.html"]Link Name[/button]
+[button link="http://link.html" name="warning"]Link Name[/button]
+
+**Button Container**
+[buttoncon]Content[/buttoncon]
+[buttoncon name="warning"][/buttoncon]
+
+See all button name attributes at:
+http://twitter.github.io/bootstrap/base-css.html#buttons
+
+**Badges**
+[badge]1[/badge]
+[badge name="success"][/badge]
+
+**Labels**
+[label][/label]
+[label name="success"][/label]
+
+See all Labels and Badges name attributes at:
+http://twitter.github.io/bootstrap/components.html#labels-badges
+
+**Wells**
+[well]Content[/well]
+[well name="large"][/well]
+
+**Icons**
+[icon]
+[icon name="star"]
+
+See all Icon names at:
+http://fortawesome.github.io/Font-Awesome/
+
+**Back to Top**
+[back_to_top]
+
+== Screenshots ==
+N/A
+
+== Frequently Asked Questions ==
+
+= Does it work? =
+
+Yes.
+
+== Changelog ==
+
+
+= 0.4 =
+Added Label.
+Added Badges.
+Added Back to top.
+Added Line Break.
+
+= 0.1 =
+Added Buttons .
+Added Classes.
+Added Alerts.
+Added Icons.
+Added Thumbnails.
+
+= 1 =
+Launched
+
+= 2 =
+Added new features. Fixed bug.
+Added [colours].
+Added [lead].
+Added titles to [button].
+
+== Upgrade Notice ==
+N/A
---
+++ ubc-bootstrap-shortcodes.php
@@ -0,0 +1,116 @@
+<?php
+/*
+Plugin Name: UBC Bootstrap shortcodes
+Plugin URI:  http://educ.ubc.ca
+Description: A plugin that allows users to use shortcodes that utilizes some of the built in styles from twitter bootstrap.
+Version: 2.02
+Author: David Brabbins
+License: GPL2
+*/
+
+//thumbnails short with class works should work with column shortcode
+function thumbnails_shortcode( $atts, $content = null ) {
+   return '<div class="thumbnails">' . do_shortcode($content) . '</div>';
+}
+add_shortcode( 'thumbnails', 'thumbnails_shortcode' );
+
+//Div class filler for custom boxes
+function container_shortcode( $atts, $content = null ) {
+	extract( shortcode_atts( array(
+      'style' => 'class',
+      ), $atts ) );
+   return '<div class="' . esc_attr($style) . '">' . do_shortcode($content) . '</div>';
+}
+add_shortcode( 'container', 'container_shortcode' );
+
+//Span class for input color options filler for custom boxes
+function colour_shortcode( $atts, $content = null ) {
+	extract( shortcode_atts( array(
+      'option' => 'pomegranate',
+      ), $atts ) );
+   return '<span class="' . esc_attr($option) . '">' . do_shortcode($content) . '</span>';
+}
+add_shortcode( 'colour', 'colour_shortcode' );
+
+//labels shortcode
+function label_shortcode( $atts, $content = null ) {
+	extract( shortcode_atts( array(
+      'name' => 'block',
+      ), $atts ) );
+   return '<span class="label label-' . esc_attr($name) . '">' . do_shortcode($content) . '</span>';
+}
+add_shortcode( 'label', 'label_shortcode' );
+
+//alert shortcode
+function alert_shortcode( $atts, $content = null ) {
+	extract( shortcode_atts( array(
+      'name' => 'block',
+      ), $atts ) );
+   return '<div class="alert alert-' . esc_attr($name) . '">' . do_shortcode($content) . '</div>';
+}
+add_shortcode( 'alert', 'alert_shortcode' );
+
+//Well shortcode
+function well_shortcode( $atts, $content = null ) {
+	extract( shortcode_atts( array(
+      'name' => '',
+      ), $atts ) );
+   return '<div class="well well-' . esc_attr($name) . '">' . do_shortcode($content) . '</div>';
+}
+add_shortcode( 'well', 'well_shortcode' );
+
+//badge shortcode
+function badge_shortcode( $atts, $content = null ) {
+	extract( shortcode_atts( array(
+      'name' => '',
+      ), $atts ) );
+   return '<span class="badge badge-' . esc_attr($name) . '">' . do_shortcode($content) . '</span>';
+}
+add_shortcode( 'badge', 'badge_shortcode' );
+
+//link with href button shortcode
+function button_shortcode( $atts, $content = null ) {
+	extract( shortcode_atts( array(
+      'name' => 'primary',
+	  'link' => '#',
+	  'title' => do_shortcode($content)
+      ), $atts ) );
+   return '<a href="' . esc_attr($link) . '" title="' . esc_attr($title) . '" class="btn btn-' . esc_attr($name) . '">' . do_shortcode($content) . '</a>';
+}
+add_shortcode( 'button', 'button_shortcode' );
+
+//span button shortcode
+function spanbutton_shortcode( $atts, $content = null ) {
+	extract( shortcode_atts( array(
+      'name' => 'primary',
+      ), $atts ) );
+   return '<span class="btn btn-' . esc_attr($class) . '">' . do_shortcode($content) . '</span>';
+}
+add_shortcode( 'buttoncon', 'spanbutton_shortcode' );
+
+//Lead Shortcode
+function lead_shortcode( $atts, $content = null ) {
+   return '<span class="lead">' . do_shortcode($content) . '</span>';
+}
+add_shortcode( 'lead', 'lead_shortcode' );
+
+//icon button shortcode, use the icon name without the icon-. Defaults to the globe.
+function icon_shortcode( $atts ) {
+	extract( shortcode_atts( array(
+      'name' => 'plane',
+      ), $atts ) );
+   return '<i class="icon-' . esc_attr($name) . '"></i>';
+}
+add_shortcode( 'icon', 'icon_shortcode' );
+
+//Back to Top Shortcode
+function back_shortcode( $atts ) {
+   return '<div class="row-fluid ubc7-back-to-top clearfix"><div class="span4"><a href="#" title="Back to top">Back to top <span class="ubc7-arrow up-arrow grey"></span></a></div></div>';
+}
+add_shortcode( 'back_to_top', 'back_shortcode' );
+
+//Line Break Shortcode
+function break_shortcode( $atts ) {
+   return '<hr />';
+}
+add_shortcode( 'break', 'break_shortcode' );
